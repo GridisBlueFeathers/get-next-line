7a234296f46500a86d963aa970032a17b3f9e629
Serhii Veretennikov <gridis72@gmail.com>
2024-04-17 19:48:48 +0200

    feat: seemingly found the problem

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:25:04 by svereten          #+#    #+#             */
-/*   Updated: 2024/04/17 17:48:21 by svereten         ###   ########.fr       */
+/*   Updated: 2024/04/17 19:41:31 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -38,7 +38,7 @@ char	*get_next_output(char *fd_buf)
 	return (res);
 }
 
-char	*remove_output(char *fd_buf)
+char	*remove_output(char *fd_buf, char *aux_buf)
 {
 	char	*res;
 	char	*nl_location;
@@ -46,6 +46,8 @@ char	*remove_output(char *fd_buf)
 
 	if (!fd_buf)
 		return (NULL);
+	if (!aux_buf)
+		return (free_fd_buf(&fd_buf), NULL);
 	nl_location = ft_strchr(fd_buf, '\n');
 	if (!nl_location || !nl_location[1])
 		return (free_fd_buf(&fd_buf), NULL);
@@ -64,7 +66,7 @@ char	*get_next_line(int fd)
 		return (NULL);
 	aux_buf = (char *)malloc(BUFFER_SIZE + 1);
 	if (!aux_buf)
-		return (NULL);
+		return (free_fd_buf(&fd_buf), NULL);
 	bytes_read = 1;
 	while (!ft_strchr(fd_buf, '\n') && bytes_read)
 	{
@@ -76,19 +78,19 @@ char	*get_next_line(int fd)
 	}
 	free(aux_buf);
 	aux_buf = get_next_output(fd_buf);
-	fd_buf = remove_output(fd_buf);
+	fd_buf = remove_output(fd_buf, aux_buf);
 	return (aux_buf);
 }
 
-/*#include <fcntl.h>
+#include <fcntl.h>
 int main()
 {
 	char *res;
-	int fd = open("./file_to_read", O_RDONLY);
+	int fd = open("./multiple_nl.txt", O_RDONLY);
 	res = get_next_line(fd);
-	while (res)
+	int i = 0;
+	while (i < 5)
 	{
-		int i = 0;
 		while (0)
 		{
 			printf("%d\n", res[i]);
@@ -97,27 +99,11 @@ int main()
 		printf("line is %s\n", res);
 		free(res);
 		res = get_next_line(fd);
-	}
-	printf("line is %s\n", res);
-	free(res);
-	close(fd);
-	fd = open("./test-files/41_no_nl", O_RDONLY);
-	res = get_next_line(fd);
-	while (res)
-	{
-		int i = 0;
-		while (0)
-		{
-			printf("%d\n", res[i]);
-			i++;
-		}
-		printf("line is %s\n", res);
-		free(res);
-		res = get_next_line(fd);
+		i++;
 	}
 	printf("line is %s\n", res);
 	free(res);
 
 	//get_next_line(3);
 	//get_next_line(3);
-}*/
+}
