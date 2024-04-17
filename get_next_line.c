6aaa1f10c0640223a06074fcdc1fcc7856c3cd13
Serhii Veretennikov <gridis72@gmail.com>
2024-04-17 17:51:56 +0200

    chore: split files and fixed norminette errors

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:25:04 by svereten          #+#    #+#             */
-/*   Updated: 2024/04/17 17:31:54 by svereten         ###   ########.fr       */
+/*   Updated: 2024/04/17 17:48:21 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -15,80 +15,6 @@ void	free_fd_buf(char **fd_buf)
 {
 	free(*fd_buf);
 	*fd_buf = NULL;
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int		i;
-
-	i = 0;
-	if (!s)
-		return (NULL);
-	while (s[i] && s[i] != (unsigned char)c)
-		i++;
-	if (!s[i] && (unsigned char)c)
-		return (NULL);
-	return (&((char *)s)[i]);
-}
-
-char	*ft_strjoin(char *fd_buf, char *aux)
-{
-	char	*res;
-	size_t	len;
-	size_t	fd_buf_len;
-	size_t	i;
-
-	if (!aux)
-		return (NULL);
-	fd_buf_len = 0;
-	if (fd_buf)
-		fd_buf_len = ft_strlen(fd_buf);
-	len = fd_buf_len + ft_strlen(aux);
-	res = (char *)malloc((len + 1) * sizeof(char));
-	if (!res)
-		return (free_fd_buf(&fd_buf), NULL);
-	i = 0;
-	while (fd_buf && fd_buf[i])
-	{
-		res[i] = fd_buf[i];
-		i++;
-	}
-	while (aux[i - fd_buf_len])
-	{
-		res[i] = aux[i - fd_buf_len];
-		i++;
-	}
-	res[i] = '\0';
-	return (free_fd_buf(&fd_buf), res);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char			*res;
-	unsigned int	new_len;
-	unsigned int	i;
-	size_t			s_len;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		new_len = 0;
-	else if (len > s_len - start)
-		new_len = s_len - start;
-	else
-		new_len = len;
-	res = (char *)malloc((new_len + 1) * sizeof(char));
-	if (!res)
-		return (NULL);
-	i = start;
-	while ((i < len + start) && s[i] && start < s_len)
-	{
-		res[i - start] = s[i];
-		i++;
-	}
-	res[i - start] = '\0';
-	return (res);
 }
 
 char	*get_next_output(char *fd_buf)
@@ -121,11 +47,9 @@ char	*remove_output(char *fd_buf)
 	if (!fd_buf)
 		return (NULL);
 	nl_location = ft_strchr(fd_buf, '\n');
-	if (!nl_location)
+	if (!nl_location || !nl_location[1])
 		return (free_fd_buf(&fd_buf), NULL);
 	nnl_index = nl_location - fd_buf;
-	if (!fd_buf[nnl_index + 1])
-		return (free_fd_buf(&fd_buf), NULL);
 	res = ft_substr(fd_buf, nnl_index + 1, ft_strlen(fd_buf) - nnl_index - 1);
 	return (free_fd_buf(&fd_buf), res);
 }
@@ -145,7 +69,6 @@ char	*get_next_line(int fd)
 	while (!ft_strchr(fd_buf, '\n') && bytes_read)
 	{
 		bytes_read = read(fd, aux_buf, BUFFER_SIZE);
-		printf("fd_buf %s\n", fd_buf);
 		if (bytes_read == -1)
 			return (free(aux_buf), free_fd_buf(&fd_buf), NULL);
 		aux_buf[bytes_read] = '\0';
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:27:03 by svereten          #+#    #+#             */
-/*   Updated: 2024/04/16 17:08:58 by svereten         ###   ########.fr       */
+/*   Updated: 2024/04/17 17:47:41 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
@@ -20,9 +20,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char *fd_buf, char *aux);
 char	*ft_strchr(const char *s, int c);
+void	free_fd_buf(char **fd_buf);
 char	*get_next_line(int fd);
 
 #endif // !GET_NEXT_LINE_H
